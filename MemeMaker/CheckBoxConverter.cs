2cc7644f3d2ee8ed5e0a3a45fffa99de5f5e63f4
alumno <alumno@A302PC35>
2022-11-02 11:22:52 +0100

    Agregar archivos de proyecto.

---
+++ MemeMaker/CheckBoxConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MemeMaker
+{
+    public class CheckBoxConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Thickness v;
+            switch (value is bool) {
+                case true:
+                    v = new Thickness(1);
+                    return v;
+                case false:
+                    v = new Thickness(0);
+                    return v;
+                default:
+                    return v = new Thickness(1);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
